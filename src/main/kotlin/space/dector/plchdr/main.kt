f7b423c12d8c870fc7bbd60a5d8162387a8b77a5
Denys M <dector@dector.space>
2021-01-10 14:26:39 +0400

    Add `/width/height` endpoint

--- src/main/kotlin/space/dector/plchdr/main.kt
+++ src/main/kotlin/space/dector/plchdr/main.kt
@@ -1,16 +1,25 @@
 package space.dector.plchdr
 
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
 import org.http4k.server.Netty
 import org.http4k.server.asServer
 
 
 fun main() {
-    val app = { req: Request ->
-        Response(Status.OK).body(req.toString())
-    }
+    val app = routes(
+        "/{width}/{height}" bind GET to { req: Request ->
+            val w = req.path("width")?.toIntOrNull() ?: 100
+            val h = req.path("height")?.toIntOrNull() ?: w
+
+            Response(OK).body("$w-$h")
+        }
+    )
 
     val server = app.asServer(Netty())
     server.start().block()
