303f63e832c0bfbff26ebe51e49f5d8bb33dc160
Denys M <dector@dector.space>
2021-01-10 20:08:20 +0400

    Make color argument optional

--- src/main/kotlin/space/dector/plchdr/main.kt
+++ src/main/kotlin/space/dector/plchdr/main.kt
@@ -14,19 +14,21 @@ import org.jetbrains.skija.Surface
 
 
 fun main() {
+    val handler = { req: Request ->
+        val w = req.path("width")?.toIntOrNull() ?: 100
+        val h = req.path("height")?.toIntOrNull() ?: w
+        val color = parseColor(req.path("color"))
+
+        val pngData = createPngImage(w, h, color)
+
+        Response(OK)
+            .header("Content-Type", "image/png")
+            .body(pngData.inputStream())
+    }
+
     val app = routes(
-        "/{width}/{height}/{color:[a-f0-9]*}" bind GET to { req: Request ->
-            val w = req.path("width")?.toIntOrNull() ?: 100
-            val h = req.path("height")?.toIntOrNull() ?: w
-            val color = (req.path("color")?.toLongOrNull(radix = 16) ?: 0xa78bfaL)
-                .toInt()
-
-            val pngData = createPngImage(w, h, color)
-
-            Response(OK)
-                .header("Content-Type", "image/png")
-                .body(pngData.inputStream())
-        }
+        "/{width}/{height}" bind GET to handler,
+        "/{width}/{height}/{color:[a-f0-9]*}" bind GET to handler,
     )
 
     val server = app.asServer(Netty())
@@ -44,4 +46,21 @@ private fun createPngImage(w: Int, h: Int, color: Int): ByteArray {
     val pngData = image.encodeToData(EncodedImageFormat.PNG)!!
 
     return pngData.bytes
+}
+
+
+const val DefaultColor = 0xa78bfa
+
+private fun parseColor(str: String?): Int {
+    str ?: return DefaultColor
+
+    val colorString = when (str.length) {
+        1 -> str.repeat(6)
+        2 -> str.repeat(3)
+        3 -> str.map { "$it$it" }.joinToString("")
+        6 -> str
+        else -> ""
+    }
+
+    return colorString.toIntOrNull(radix = 16) ?: DefaultColor
 }
